22bcc25370c0edb2da001b20118896c45f43879c
kaplishjs <kaplishjs@gmail.com>
2023-08-24 19:47:44 +0530

    conig file

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config, { isServer }) {
+      const prefix = config.assetPrefix ?? config.basePath ?? "";
+      config.module.rules.push({
+        test: /\.(mp4|mov|m4v|pdf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              publicPath: `${prefix}/_next/static/media/`,
+              outputPath: `${isServer ? "../" : ""}static/media/`,
+              name: "[name].[hash].[ext]",
+            },
+          },
+        ],
+      });
+  
+   
+  
+      return config;
+    },
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+   
+  
+  module.exports = nextConfig;
