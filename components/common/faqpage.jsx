17e0dadd6908c85ad7cf26c5b4580f3b0dd0aa74
kaplishjs <kaplishjs@gmail.com>
2023-09-05 22:03:21 +0530

    faq

--- components/common/faqpage.jsx
+++ components/common/faqpage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function faqpage() {
+export default function Faqpage() {
 	return (
        
 		<>
---
+++ pages/faq.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Faqpage from '../components/common/faqpage'
+
+function faq() {
+  return (
+    <Faqpage/>
+  )
+}
+
+export default faq
